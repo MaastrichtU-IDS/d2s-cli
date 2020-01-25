60eb9e3006c1d0bd4500d00be78bce3f5e8d3ed2
Vincent Emonet <vincent.emonet@gmail.com>
2020-01-25 11:09:52 +0100

    now store config in a proper config file .d2sconfig using configparser lib

--- d2s.py
+++ d2s.py
@@ -1,5 +1,6 @@
 import click
 import os
+import configparser
 import cwltool.factory
 
 @click.group()
@@ -8,49 +9,67 @@ def cli():
 
 
 @cli.command()
-# @click.option(
-#     '--workspace', '-w',
-#     help='Path to the working directory. Default: /data/d2s-workspace',
-# )
 def init():
+    """Create workspace dir and download workflows examples in current dir"""
+    config = configparser.ConfigParser()
     workspace = click.prompt('Enter the absolute path to the working directory. Default', default='/data/d2s-workspace')
-    os.system('echo "' + workspace + '" > .d2s')
+    config['local'] = {'Workspace': workspace}
+
     click.echo('[ Create ' + workspace + ' ] -- Your password might be required to set ownerships')
     os.system('sudo mkdir -p ' + workspace)
     os.system('sudo chown -R ${USER} ' + workspace)
+
     if click.confirm('Do you want to download the template workflows and datasets?'):
         os.system('git clone --recursive https://github.com/MaastrichtU-IDS/d2s-transform-template.git .')
-    click.echo('[ Your Data2Services environment has been stored in the .d2s file ]')
+        # Remove link to the GitHub repo and create new repo?
+        # os.system('rm -rf .git && git init && git add . && git commit -m "d2s init"')
+    
+    with open('.d2sconfig', 'w') as configfile:
+        config.write(configfile)
+    click.echo('[ Your d2s environment config has been stored in the .d2sconfig file ]')
 
 @cli.command()
 def update():
+    """Update Data2Services docker images"""
     os.system('docker-compose -f d2s-cwl-workflows/docker-compose.yaml pull')
     click.echo('All images pulled.')
 
 
 @cli.command()
 @click.argument('services', nargs=-1)
-# @click.option(
-#     '--api-key', '-a',
-#     help='your API key for the OpenWeatherMap API',
-# )
 def start(services):
-    for service in services:
-        click.echo(service)
-    # os.system('docker-compose -f d2s-cwl-workflows/docker-compose.yaml up -d --build --force-recreate virtuoso drill graphdb browse-local-virtuoso browse-local-graphdb')
-    print('started!!')
+    """Start services (databases, interfaces)"""
+    # for service in services:
+    #     click.echo(service)
+    services_string = " ".join(services)
+    print(services_string)
+    os.system('docker-compose -f d2s-cwl-workflows/docker-compose.yaml up -d --build --force-recreate ' + services_string)
+    click.echo('[ ' + services_string + ' started ]')
+
+
+@cli.command()
+@click.argument('services', nargs=-1)
+@click.option(
+    '--all/--no-all', default=False, 
+    help='Stop all services')
+def stop(services, all):
+    """Stop services (--all to stop all services)"""
+    if all:
+        os.system('docker-compose -f d2s-cwl-workflows/docker-compose.yaml down')
+        click.echo('[ All services stopped ]')
+    else:
+        services_string = " ".join(services)
+        os.system('docker-compose -f d2s-cwl-workflows/docker-compose.yaml stop ' + services_string)
+        click.echo('[ ' + services_string + ' stopped ]')
 
 
 @cli.command()
 @click.argument('workflow')
 @click.argument('dataset')
-# @click.option(
-#     '--api-key', '-a',
-#     help='your API key for the OpenWeatherMap API',
-# )
 def run(workflow, dataset):
+    """Run CWL workflows"""
     # cwl_factory = cwltool.factory.Factory()
     # run_workflow = cwl_factory.make(workflow) # the .cwl file
     # result = run_workflow(inp=dataset)  # the config yaml
-    print('started!!')
+    print('Running!!')
 
