1081b3dbf4cf87182b9394846d5c98f04bda5c71
Vincent Emonet <vincent.emonet@gmail.com>
2020-02-01 09:43:31 +0100

    clean setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
-from setuptools import setup, Extension
+from setuptools import setup
 import setuptools
-import codecs
-
-# with codecs.open("README.md", "r", "utf-8") as file:
-#     long_description = file.read()
 
 setup(
     name='d2s',
     version='0.1.8',
-    license="MIT license",
+    license="MIT License",
     url="https://github.com/MaastrichtU-IDS/d2s-cli",
     author="Vincent Emonet",
     author_email="vincent.emonet@gmail.com",
     description="A Command Line Interface to orchestrate the integration of heterogenous data and the deployment of services consuming the integrated data. See https://d2s.semanticscience.org",
-    long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
-    # long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     py_modules=['d2s'],
     install_requires=[
