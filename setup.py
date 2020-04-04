4030ab83b23856fac5b4ec13254e36cba1f741e2
Vincent Emonet <vincent.emonet@gmail.com>
2020-04-04 18:02:41 +0200

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='d2s',
-    version='0.1.27',
+    version='0.1.28',
     license="MIT License",
     url="https://github.com/MaastrichtU-IDS/d2s-cli",
     author="Vincent Emonet",
