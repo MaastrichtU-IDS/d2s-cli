a06914ae621fe7757f23e7779dd08ec3d93de134
Vincent Emonet <vincent.emonet@gmail.com>
2020-04-27 07:18:14 +0200

    upgrade d2s version to 32

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='d2s',
-    version='0.1.31',
+    version='0.1.32',
     license="MIT License",
     url="https://github.com/MaastrichtU-IDS/d2s-cli",
     author="Vincent Emonet",
