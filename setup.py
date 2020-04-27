75d23698586e6ced5d5771fad6cd883ebe2b77d8
Vincent Emonet <vincent.emonet@gmail.com>
2020-04-27 11:26:07 +0200

    upgrade version to 0.2.1 (change submodules from d2s-cwl-workflows and d2s-argo-workflows to d2s-core)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='d2s',
-    version='0.1.32',
+    version='0.2.1',
     license="MIT License",
     url="https://github.com/MaastrichtU-IDS/d2s-cli",
     author="Vincent Emonet",
