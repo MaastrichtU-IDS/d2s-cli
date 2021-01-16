ce81635a7f36d19571e1d9b7eb7fdadbb57d0c9f
Vincent Emonet <vincent.emonet@gmail.com>
2021-01-16 17:00:39 +0100

    rename d2s.py to cli.py and fix tests

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=open("requirements.txt", "r").readlines(),
     entry_points={
         'console_scripts': [
-            'd2s=d2s.d2s:cli',
+            'd2s=d2s.cli:cli',
         ],
     },
     classifiers=[
