17de950ced3b3472114c3efb216e4c35732c3eac
Vincent Emonet <vincent.emonet@gmail.com>
2021-02-27 01:58:15 +0100

    fix tests

--- tests/test_d2s.py
+++ tests/test_d2s.py
@@ -4,7 +4,7 @@ import os.path
 
 def test_d2s():
    runner = CliRunner()
-   result = runner.invoke(d2s.init, ['d2s-test-project'], input='\n\n')
+   result = runner.invoke(d2s.init, [], input='\n')
    assert not result.exception
    assert os.path.isfile('README.md')
 #    assert result.output == 'Foo: wau wau\nfoo=wau wau\n'
