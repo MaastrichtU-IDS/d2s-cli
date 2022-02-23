7c1618fd54fc8462867b166af44669760f3d39dc
Vincent Emonet <vincent.emonet@gmail.com>
2022-02-23 20:34:31 +0100

    upgrade to 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setup(
     name='d2s',
-    version='0.3.1',
+    version='0.3.2',
     license="MIT License",
     url="https://github.com/MaastrichtU-IDS/d2s-cli",
     author="Vincent Emonet",
